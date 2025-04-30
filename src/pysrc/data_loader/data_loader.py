52d8049b8665824b0c016d4981101f3fd3c02f9b
William Brittian <williambrittian13@gmail.com>
2025-04-30 14:15:20 -0500

    switched up file structure

---
+++ src/pysrc/data_loader/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class DataLoader:
+    def __init__() -> None:
+        pass
+
+    def load_data() -> None:
+        pass
+
+    def load_tokens() -> dict[int, str]:
+        pass
