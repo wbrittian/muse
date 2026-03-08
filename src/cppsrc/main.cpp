a13ac7cd60ef412741b504b8d18a0b0d9dc0d078
William Brittian <williambrittian13@gmail.com>
2026-03-08 18:21:15 -0500

    add pybind11 bindings and fix CMakeLists sources

--- src/cppsrc/main.cpp
+++ src/cppsrc/main.cpp
@@ -1,5 +1,20 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
 #include "model/museformer.hpp"
 
-int main() {
-    return 0;
+namespace py = pybind11;
+
+PYBIND11_MODULE(model, m) {
+    py::class_<Museformer>(m, "Museformer")
+        .def(py::init<int, int, int, int, int, int>(),
+             py::arg("vocab_size"),
+             py::arg("max_seq_len"),
+             py::arg("d_model"),
+             py::arg("num_heads"),
+             py::arg("num_layers"),
+             py::arg("dim_ff"))
+        .def("load",     &Museformer::load)
+        .def("save",     &Museformer::save)
+        .def("generate", &Museformer::generate);
 }
